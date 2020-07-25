0134cda69917dcecdafd32a34b5d28977f5c6944
Tanosimboangy <jacquit.aja@onja.org>
2020-07-25 11:43:17 +0300

    adding some comments

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const newInput = document.querySelector('.new-input');
 const newStep = document.querySelector('.new-step');
 const submitButton = document.querySelector('#submit-button');
 
+// ********* // *********** //
+// Adding new list for the ingredients list
 const handleClick = event => {
     console.log(event);
     const myHTML = `
@@ -20,11 +22,21 @@ const handleClick = event => {
 } 
 newIngredient.addEventListener('click', handleClick);
 
+// ********** // ********* //
+// Adding new lists for the steps
 const handleButtonClick = event => {
     console.log(event);
     const mySecHTML = `
     <input type="text" id="new-input-step" placeholder="Put the steps to prepare this recipe" required ><br>
     `;
     newStep.innerHTML += mySecHTML;
-} 
+}
+
 addingStepsButton.addEventListener('click', handleButtonClick);
+
+// ********** // *********** //
+// Manupilating the submit button
+const handleSubmitButton = (event) => {
+    console.log(event);
+}
+submitButton.addEventListener('click', handleSubmitButton);
